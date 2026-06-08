7082b987449d3a25eabc3495fe97de4ac2486a45
HaIWaeR <vitalik.papkov@mail.ru>
2026-06-08 17:04:33 +0300

    add file floder and README.md

---
+++ _4AccessModifiers/file/FileTrain/FileTrain/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace FileTrain
+{
+    file class Car
+    {
+        public string model = "Toyota";
+
+        public void ShowInfo() => Console.WriteLine($"Модель: {model}");
+    }
+
+    public class NewsOnTV
+    {
+        public void ShowInfoCar()
+        {
+            Car myCar = new Car();
+            Console.WriteLine($"Вышла новая модель: {myCar.model}");
+        } 
+    }
+}
---
+++ _4AccessModifiers/file/FileTrain/FileTrain/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileTrain
+{
+    // file - работает только в файле 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NewsOnTV newsOnTV = new NewsOnTV();
+
+            newsOnTV.ShowInfoCar();
+            
+            // Car myCar = new Car();
+            // Console.WriteLine(Car.ShowInfo());
+
+            // Ошибка CS9059: Тип "Car" объявлен как file и не виден за пределами файла
+        }
+    }
+}
