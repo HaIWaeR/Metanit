1c8899c991f76e1326dc2127df588486b03ac1d3
HaIWaeR <vitalik.papkov@mail.ru>
2026-07-03 23:57:32 +0300

    add Type conversion

---
+++ _10TypeСonversion/TypeСonversion/Animals.cs
@@ -0,0 +1,32 @@
+﻿namespace TypeСonversion
+{
+    public class Animal
+    {
+        public string Group { get; set; }
+
+        public Animal(string nameGroup)
+        {
+            Group = nameGroup;
+        }
+
+    }
+    public class Dog : Animal
+    {
+        public string Name { get; set; }
+
+        public Dog(string name, string group) : base(group)
+        {
+            Name = name;
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Name { get; set; }
+
+        public Cat(string name, string group) : base(group)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ _10TypeСonversion/TypeСonversion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace TypeСonversion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Tom");
+            Employee employee = new Employee("Tom", "Microsoft");
+
+            Person person2 = employee; // Входящее преобразование 
+
+            Person bob = new Client("Bob", "Tbank"); // Тоже восходящее преобразоввание
+
+
+            Person tom = new Person("tom");
+            Employee? emp = person as Employee;
+
+            if (emp == null)
+                Console.WriteLine("Это не сотрудник");
+            else
+                Console.WriteLine($"Сотрудник рабоатет в {emp.Company}");
+
+
+
+
+
+            List<Animal> AnimalList = new List<Animal>();
+
+            Animal animal = new Animal("Животное");
+            Dog dog = new Dog("Собака", "Животное");
+            Cat cat = new Cat("Кот", "Животное");
+
+            AnimalList.Add(cat);
+            AnimalList.Add(animal);
+            AnimalList.Add(dog);
+
+            for (int i = 0; i < AnimalList.Count; i++)
+            {
+                if (AnimalList[i] is Dog)
+                {
+                    Console.WriteLine("Гав!");
+                }
+                else if (AnimalList[i] is Cat)
+                {
+                    Console.WriteLine("Мяу!");
+                }
+                else if (AnimalList[i] is Animal)
+                {
+                    Console.WriteLine("Животное");
+                }
+            }
+        }
+    }
+}
